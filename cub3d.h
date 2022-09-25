e322ed087c23cf063d0c5bcc1987e10929917458
ilahyani <lahyani799@gmail.com>
2022-09-25 11:43:17 +0100

    include local mlx header

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 11:47:34 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/09/23 11:50:54 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/09/25 11:02:10 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,21 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <math.h>
-# include <mlx.h>
+# include "minilibx/mlx.h"
 # include "libft/libft.h"
+
+typedef struct  s_image {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_image;
+
+typedef struct s_vector
+{
+	double	x;
+	double	y;
+}	t_vector;
 
 #endif
